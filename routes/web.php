a8c0c83cbba294ae2fae682b6cf811d739975819
HS-code0 <91959256+HS-code0@users.noreply.github.com>
2021-12-05 22:25:06 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'ProfileController@add');
+    Route::get('profile/edit', 'ProfileController@edit');
+});
+?>
